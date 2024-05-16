08bf8b1691e12375a3128a029ca79ac2b4c5a653
soumyaaaaaaaaaaaaaaaaaaaaaaa <167617017+soumyaaaaaaaaaaaaaaaaaaaaaaa@users.noreply.github.com>
2024-05-16 16:30:42 +0530

    methodss

--- src/Test.java
+++ src/Test.java
@@ -8,8 +8,8 @@ public class Test {
     }
     public static void main(String args[])
     {
-        int a=10,b=15,c;
-        c=max(a,b);
-        System.out.println(c);
+        int a=10,b=15;
+
+        System.out.println(max(a,b));
     }
 }
